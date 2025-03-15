55e483f103bd418d9124f0077b26da035660e55d
josnnydev <pjosnny@gmail.com>
2025-03-15 13:34:47 -0400

    changes334555

--- app/products/[Id]/page.tsx
+++ app/products/[Id]/page.tsx
@@ -1,27 +1,28 @@
-'use server';
 import { getProductById } from '@/prisma-db';
 
 
 export default async function ProductsId({
-    params,
-  }: {
-    params: Promise<{ id: string }>
-  }) {
-    const { id } = await params;
-    const product = await getProductById(Number(id))
+  params,
+}: {
+  params: { id: string };
+}) {
+  // Accede directamente a params.id
+  const product = await getProductById(Number(params.id));
 
-    
-
-     
-
+  // Validación en caso de que el producto no exista
+  if (!product) {
     return (
-        <div className="container mx-auto px-4 py-8">
-            <h1>{product?.name}</h1>
-            <p>{product?.description}</p>
-            <p>{product?.price}</p>
-            
-        </div>
+      <div className="container mx-auto px-4 py-8">
+        <h1>Producto no encontrado</h1>
+      </div>
     );
+  }
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+    </div>
+  );
 }
-   
- 
