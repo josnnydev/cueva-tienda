a6d217245e3d8249e707eee47edaae1f274669b7
josnnydev <pjosnny@gmail.com>
2025-03-17 18:10:01 -0400

    added contacts

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,34 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
+import {usePathname} from 'next/navigation'
+import { FaCartShopping } from "react-icons/fa6";
 
 export default function Header() {
+  const pathname = usePathname();
   return (
     <div className="bg-gray-800 text-white p-4">
-        <nav className="container mx-auto">
-            <ul className="flex justify-between">
-                <li className="mr-4"><Link href="/">Home</Link></li>
-                <li className="mr-4"><Link href="/products">Products</Link></li>
-                <li className="mr-4"><Link href="/contactos">Contactos</Link></li>
-                
-                <li className="mr-4 " title='soon'><Link href="/#">Carrito</Link></li>
-            </ul>
-        </nav>
+    <nav className="flex space-x-4 bg-gray-900 p-4 text-white">
+      <NavItem href="/" pathname={pathname}>Inicio</NavItem>
+      <NavItem href="/products" pathname={pathname}>Productos</NavItem>
+      <NavItem href="/contactos" pathname={pathname}>Contacto</NavItem>
+      <div className=" ml-auto " title='soon'>
+      <NavItem  href="/#" pathname={pathname}> <FaCartShopping className="text-2xl" /> </NavItem>
+      </div>
+    </nav>
     </div>
   )
+}
+
+function NavItem({ href, pathname, children }: { href: string, pathname: string, children: React.ReactNode }) {
+  const isActive = pathname === href;
+
+  return (
+    <Link
+      href={href}
+      className={`px-3 py-2 rounded-lg ${isActive ? "bg-blue-500" : "hover:bg-gray-700"}`}
+    >
+      {children}
+    </Link>
+  );
 }
--- app/contactos/page.tsx
+++ app/contactos/page.tsx
@@ -1,21 +1,31 @@
 import Link from 'next/link'
 import Image from 'next/image' 
+import { FaWhatsapp } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io";
+import { FaFacebook } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
 
 export default function Contactos() {
   return (
     <div className="flex flex-col items-center justify-center  ">
 
-      <div className="mb-4">
+      <div className="mb-4 mt-4">
         
-        <Link href="https://wa.me/message/P3LU5C3LG4JPA1" target="_blank">Whatsapp</Link>
-        <Image src="#" alt="Whatsapp" width={32} height={32} />
+        
+        <Image src="/images/whatsapp.webp" alt="Whatsapp" width={300} height={50} />
       </div>
-      <div className="mb-4">
-        Redes Sociales
+      <div className="flex flex-col gap-4 items-center justify-center mb-4">
+      <strong>Redes Sociales</strong>
+        <Link href="https://wa.me/message/P3LU5C3LG4JPA1" target="_blank"><FaWhatsapp className="text-4xl text-green-600 hover:text-black" /></Link>
+        <Link href="https://www.instagram.com/cueva.boutique/" target="_blank"> <IoLogoInstagram className='text-6xl text-red-500 hover:text-yellow-500' />  </Link>
+        <Link href="https://www.facebook.com/cueva.boutique/" target="_blank"> <FaFacebook className='text-6xl text-blue-500 hover:text-red-500'/></Link>
+        <Link href="https://www.tiktok.com/@cueva.boutique" target="_blank"> <FaTiktok className='text-6xl text-black-500   hover:text-pink-500  '/> </Link>
+
       </div>
 
-    <h2>Ubicación</h2>
-      <div className="  ">
+    
+      <div className="flex flex-col gap-4 items-center justify-center mb-4 mt-4">
+      <strong>Ubicación</strong>
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d945.9886918312812!2d-69.89639963008881!3d18.48570796629716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaf893b69456ec5%3A0x9ebf9f1a88bd1d1c!2scueva%20boutique!5e0!3m2!1ses-419!2sdo!4v1742130483512!5m2!1ses-419!2sdo" width="600" height="200" style={{border:0}} allowFullScreen loading="lazy"referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
