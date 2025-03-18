121bf6755f10e5f4756c66c8d8e0ddab7c362a9d
josnnydev <pjosnny@gmail.com>
2025-03-18 17:37:07 -0400

    updated map

--- app/contactos/page.tsx
+++ app/contactos/page.tsx
@@ -9,7 +9,7 @@ export default function Contactos() {
   return (
     <div className="flex flex-col items-center justify-center  ">
 
-      <div className="mb-4 mt-4">
+      <div className="mb-4 mt-4 ">
         
         
         <Image src="/images/whatsapp.webp" alt="Whatsapp" width={300} height={50} />
@@ -26,7 +26,7 @@ export default function Contactos() {
     
       <div className="flex flex-col gap-4 items-center justify-center mb-4 mt-4">
       <strong>Ubicación</strong>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d945.9886918312812!2d-69.89639963008881!3d18.48570796629716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaf893b69456ec5%3A0x9ebf9f1a88bd1d1c!2scueva%20boutique!5e0!3m2!1ses-419!2sdo!4v1742130483512!5m2!1ses-419!2sdo" width="600" height="200" style={{border:0}} allowFullScreen loading="lazy"referrerPolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d945.9886918312812!2d-69.89639963008881!3d18.48570796629716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaf893b69456ec5%3A0x9ebf9f1a88bd1d1c!2scueva%20boutique!5e0!3m2!1ses-419!2sdo!4v1742130483512!5m2!1ses-419!2sdo" width="410" height="200" style={{border:0}} allowFullScreen loading="lazy"referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
   )
